34dfa487f14b57da894b42c0c664467ae2c7daca
KhrAini <ayni110603@gmail.com>
2024-10-13 23:11:23 +0700

    update tugas praktikum

---
+++ 15. React Hook/praktikum/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import EditProduct from './EditProduct';
+const CreateProduct = () => {
+  return (
+    <>
+      <Header />
+      <EditProduct/>
+    </>
+  );
+};
+
+export default CreateProduct;
